505cd867d0afe1daf19d110be395244b848f7606
Styx Meiseles <git@0styx0.xyz>
2025-10-28 15:56:24 -0400

    refactor(utils): separate fns

--- utils.ts
+++ utils.ts
@@ -1,26 +1,36 @@
 import { FillableJar } from "./components/fillable-jar/fillable-jar.js";
-import { defaultKinks } from "./kinks.js";
 import {
   defaultJarAttrs,
   JarAttrs,
 } from "./components/fillable-jar/jarAttrs.js";
+import { defaultJars } from "./defaultJars.js";
+
+const selectors = {
+  jarGrid: "#jarGrid",
+  addJar: "add-jar",
+};
 
 export function createDefaultJars() {
-  const jarGrid = queryElt(document, "#jarGrid");
-  const addJar = queryElt(document, "add-jar");
+  const jarGrid = queryElt(document, selectors.jarGrid);
+  const addJar = queryElt(document, selectors.addJar);
 
+  const jarElts = createJarElts(defaultJars);
+
+  jarGrid?.insertBefore(jarElts, addJar);
+}
+
+function createJarElts(jarAttrsList: Partial<JarAttrs>[]) {
   const fragment = document.createDocumentFragment();
 
-  defaultKinks.forEach((item: JarAttrs) => {
+  jarAttrsList.forEach((item) => {
     const newJar = createFillableJar({
       ...defaultJarAttrs,
-      label: item.label,
+      ...item,
     });
 
     fragment.appendChild(newJar);
   });
-
-  jarGrid.insertBefore(fragment, addJar);
+  return fragment;
 }
 
 export function createFillableJar(prefs: JarAttrs) {
@@ -40,14 +50,14 @@ export function queryElt<Elt extends Element>(
 ) {
   if (!container) {
     console.warn("Unable to find container ", { container, selector });
-    return;
+    return null;
   }
 
   const elt = container.querySelector<Elt>(selector);
 
   if (!elt) {
     console.warn("Unable to find element ", selector);
-    return;
+    return null;
   }
 
   return elt;
