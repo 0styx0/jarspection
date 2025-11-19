3b5601973e73846dc7c43bbdfad42163c0f0f651
Styx Meiseles <git@0styx0.xyz>
2025-11-19 10:06:26 -0500

    chore: omit e2e tests from vitest unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
+import { configDefaults, defineConfig } from "vitest/config";
+
 export default {
   test: {
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
+    exclude: [...configDefaults.exclude, "e2e-tests/"],
     isolate: true,
   },
 };
